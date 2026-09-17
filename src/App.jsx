119ba93f3feef1214b2125a5f7bcc20b3352b443
Ayesha Jafer <ayeshajafer699@gmail.com>
2026-09-17 19:28:38 +0500

    Added Authentication

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,20 @@ import Home from "./pages/Home";
 import Auth from "./pages/Auth";
 import Checkout from "./pages/Checkout";
 import Navbar from "./components/Navbar";
+import  AuthProvider  from "./context/Authcontext";
 
 function App() {
 
   return <div className="app">
-    
+    <AuthProvider>
     <Navbar />
 
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/auth" element={<Auth />}/>
       <Route path="/checkout" element={<Checkout />}/>
-    </Routes>
+      </Routes>
+      </AuthProvider>
   </div>;
 }
 
